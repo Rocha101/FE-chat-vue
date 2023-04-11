d24208435223932e31d3e00c68454e50067f0d8c
rocha101 <eduardoprudenciorocha@gmail.com>
2023-04-11 20:50:00 -0300

    up

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+    <a-row justify="center">
+        <a-row style="width: 60ch;" :gutter="5" justify="center">
+            <a-col span="20">
+                <a-input placeholder="Sua mensagem" />
+            </a-col>
+            <a-col span="4">
+                <a-button type="primary">Enviar</a-button>
+            </a-col>
+        </a-row>
+    </a-row>
+</template>
