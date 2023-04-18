0429716858a997c57c4323f38b1ad50d3ee7dbba
rocha101 <eduardoprudenciorocha@gmail.com>
2023-04-18 19:44:42 -0300

    Responsividade

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -69,15 +69,15 @@ function handleModo(){
                 </p>
             </div>
         </div>
-        <form class="bg-white dark:bg-gray-700 p-4 flex items-center">
+        <form class="bg-white dark:bg-gray-700 p-1 md:p-4 flex items-center">
             <input type="text"
-                class="flex-1 rounded-full border-gray-100 border-2 px-4 py-2 mr-2 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-900 dark:border-gray-500 dark:text-gray-100"
+                class="flex-1 rounded-full border-gray-100 border-2 px-3 py-1 md:px-4 md:py-2 mr-2  focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-900 dark:border-gray-500 dark:text-gray-100"
                 placeholder="Digite sua mensagem..." @change="handleChange" :value="mensagem.texto">
             <button type="submit" @click="enviarMensagem"
                 class="inline-flex items-center px-4 py-2 mr-2 border border-transparent rounded-full font-semibold text-white dark:text-gray-100 bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Enviar
             </button>
-            <button @click="handleModo" class="inline-flex items-center px-4 py-2 border border-transparent rounded-full font-semibold text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button @click="handleModo" class="hidden md:inline-flex items-center px-4 py-2 border border-transparent rounded-full font-semibold text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 {{
                     modo === 'dark' ? '🌞' : '🌙'
                 }}
